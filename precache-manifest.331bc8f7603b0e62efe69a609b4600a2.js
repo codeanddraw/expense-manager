ad1f9fa7c6419a15581a7a96b495c695555fbb15
Nisha <nisha@Nishas-MacBook-Pro.local>
2020-10-14 14:00:59 -0400

    Updates

---
+++ precache-manifest.331bc8f7603b0e62efe69a609b4600a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6488289dbbe04d9e96c6e2c703cffb68",
+    "url": "/reponame/index.html"
+  },
+  {
+    "revision": "7008ec67e21cee71d25e",
+    "url": "/reponame/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "ad5ccdf72078a970540b",
+    "url": "/reponame/static/css/main.f379ea6d.chunk.css"
+  },
+  {
+    "revision": "7008ec67e21cee71d25e",
+    "url": "/reponame/static/js/2.959525db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reponame/static/js/2.959525db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5ccdf72078a970540b",
+    "url": "/reponame/static/js/main.8211f640.chunk.js"
+  },
+  {
+    "revision": "bdd0ac920e640849841c",
+    "url": "/reponame/static/js/runtime-main.5ff1af57.js"
+  }
+]);
